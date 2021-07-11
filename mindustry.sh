3307847e9697a862f06ef776fc0b459e8a699daf
Hari Rana <theevilskeleton@riseup.net>
2021-07-11 15:54:10 -0400

    Add support for RPC

--- mindustry.sh
+++ mindustry.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+for i in {0..9}; do
+    test -S $XDG_RUNTIME_DIR/discord-ipc-$i || ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
+    test -S $XDG_RUNTIME_DIR/discord-ipc-$i || ln -sf {app/com.discordapp.DiscordCanary,$XDG_RUNTIME_DIR}/discord-ipc-$i;
+done
+
 java -jar /app/bin/Mindustry.jar  $@
