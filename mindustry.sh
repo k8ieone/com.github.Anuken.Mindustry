68a9a120e1383b8706f32d1618912602b4785503
Hari Rana <theevilskeleton@riseup.net>
2022-11-12 14:41:52 -0500

    Update Mindustry.jar to 140.3 (#46)

--- mindustry.sh
+++ mindustry.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+for i in {0..9}; do
+    test -S $XDG_RUNTIME_DIR/discord-ipc-$i || ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
+    test -S $XDG_RUNTIME_DIR/discord-ipc-$i || ln -sf {app/com.discordapp.DiscordCanary,$XDG_RUNTIME_DIR}/discord-ipc-$i;
+done
 
 for i in {0..9}; do
     test -S $XDG_RUNTIME_DIR/discord-ipc-$i || ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
